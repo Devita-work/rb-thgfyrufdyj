48778bdefec2210b79720b1e4e943367f72930e6
Vitali Metelev <devita.work@yandex.ru>
2020-12-18 10:52:26 +0400

    Добавьте файлы проекта.

---
+++ WindowsFormsApp8/Talker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    class Talker
+    {
+        public static int BlahBlahBlah(string thingToSay, int numberOfTimes)
+        {
+           
+            string finalString = "";
+            for (int count = 1; count <= numberOfTimes; count++)
+            {
+                finalString = finalString + thingToSay + "\n";
+            }
+            MessageBox.Show(finalString);
+            return finalString.Length;
+        }
+        public static string CutBukvu(string thingToSay)
+        {
+            thingToSay = thingToSay.Substring(1);
+           
+            return thingToSay;
+        }
+    }
+}
